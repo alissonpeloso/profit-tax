bc24531311af4178f9541564a7d12ff71ee8b93f
Alisson <alisson.luan2000@gmail.com>
2024-09-20 18:29:37 -0300

    checkpoint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css';
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
+
+import SlimSelect from 'slim-select'
+window.SlimSelect = SlimSelect
