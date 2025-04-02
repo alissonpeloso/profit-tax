cbd16daaa5099d4987763367b900a42ae6f60a69
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2025-04-01 22:58:06 -0300

    adjust

--- resources/views/livewire/stock-trade-list.blade.php
+++ resources/views/livewire/stock-trade-list.blade.php
@@ -167,7 +167,7 @@
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap">
                                                 <div class="text-sm text-gray-900 dark:text-gray-200">
-                                                    {{ StockTradeClass::from($stockTrade->class)->getLabel() ?? null }}
+                                                    {{ StockTradeClass::tryFrom($stockTrade->class)?->getLabel() ?? null }}
                                                 </div>
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap">
